c8c9dca68e2364142fc1caa17644bbf70fb58ea5
FerdosFijer <ffijer@gmail.com>
2026-05-24 07:01:28 +0600

    DOne 2nd part

--- file-organizer.js
+++ file-organizer.js
@@ -95,3 +95,49 @@ function organizeFiles() {
     byCategory: {},
   };
 
+  files.forEach((file) => {
+    const sourcePath = path.join(sourceDir, file);
+    const stat = fs.statSync(sourcePath);
+    if (stat.isDirectory()) {
+      return;
+    }
+    const category = getCategory(file);
+    const destDir = path.join(organizedDir, category);
+    const destPath = path.join(destDir, file);
+
+    fs.copyFileSync(sourcePath, destPath);
+
+    stats.total++;
+    stats.byCategory[category] = (stats.byCategory[category] || 0) + 1;
+
+    console.log(`${file}`);
+    console.log(`${category}`);
+    console.log(`${stat.size}`);
+  });
+}function showHelp() {
+  console.log(`
+        file organizer - usage:
+
+        commands: 
+        init - create files
+        organize - organize files into categories
+
+        example:
+        node file-organizer init
+        node file-organizer organize
+        `);
+}
+
+const command = process.argv[2];
+
+switch (command) {
+  case "init":
+    initializeDirectories();
+    break;
+  case "organize":
+    organizeFiles();
+    break;
+  default:
+    showHelp();
+    break;
+}
